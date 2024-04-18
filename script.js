5b5856c8ec872de49abbf0e1117f1a46eac859a3
Chandru <137380053+VKIDS2004@users.noreply.github.com>
2024-04-18 21:05:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,59 @@
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const img = document.getElementById("modal-img");
 const overlay = document.getElementById('overlay')
+const modal = document.getElementById('modal')
+let isNavOpen = false;
 
-openModalButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const modal = document.querySelector(button.dataset.modalTarget)
-    openModal(modal)
-  })
-})
-
-overlay.addEventListener('click', () => {
-  const modals = document.querySelectorAll('.modal.active')
-  modals.forEach(modal => {
-    closeModal(modal)
-  })
-})
-
-closeModalButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const modal = button.closest('.modal')
-    closeModal(modal)
-  })
-})
-
-function openModal(modal) {
-  if (modal == null) return
+function openModal(m) {
+  img.src =`assets/certifications/cer-${m}.png`;
   modal.classList.add('active')
   overlay.classList.add('active')
+  console.log(m)
 }
 
-function closeModal(modal) {
-  if (modal == null) return
+function closeModal() {
+
+  isNavOpen&&openCloseNav();
   modal.classList.remove('active')
   overlay.classList.remove('active')
+}
+
+// ------------------------ goto top ---------------------------------
+let mybutton = document.getElementById("gotop");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if ( document.documentElement.scrollTop > 100) {
+    mybutton.style.zIndex = "10";
+    mybutton.style.opacity = "1";
+  } else {
+    mybutton.style.zIndex = "-2";
+    mybutton.style.opacity = "0";
+
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+// ---------------------- open nav ---------------------------------
+
+
+
+function openCloseNav(){
+  if(window.innerWidth<=768){
+    if(isNavOpen){
+      isNavOpen=false;
+      closeModal();
+      document.querySelector("#navBar").style.left="-200px";
+    }
+    else{
+      isNavOpen=true;
+      openModal();
+      document.querySelector("#navBar").style.left="0";
+    }
+  }
 }
